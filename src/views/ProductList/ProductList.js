9bb2b9e435494c3bc0d5d0f3b3a97e42025441ae
Harshitha Sompura <sompharshi123@gmail.com>
2020-05-04 17:29:54 +0530

    Update ProductList.js

--- src/views/ProductList/ProductList.js
+++ src/views/ProductList/ProductList.js
@@ -3,16 +3,15 @@ import { makeStyles } from '@material-ui/styles';
 import { IconButton, Grid, Typography } from '@material-ui/core';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
 import { ProductsToolbar, ProductCard } from './components';
 import mockData from './data';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   },
   content: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   pagination: {
     marginTop: theme.spacing(3),
