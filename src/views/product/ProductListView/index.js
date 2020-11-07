b4c887f6e8e5c8d0a11d44844cd53dd01f2cf469
malyz <mattuy001@gmail.com>
2020-11-08 12:14:22 +1300

    removed placeholders

--- src/views/product/ProductListView/index.js
+++ src/views/product/ProductListView/index.js
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Container,
-  Grid,
-  makeStyles
-} from '@material-ui/core';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Box, Container, Grid, makeStyles } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 import Page from 'src/components/Page';
-import Toolbar from './Toolbar';
-import ProductCard from './ProductCard';
-import data from './data';
 
-const useStyles = makeStyles((theme) => ({
+import Toolbar from './Toolbar';
+import EventCard from './EventCard';
+import * as eventAPI from '../../../store/actions/events';
+
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     minHeight: '100%',
@@ -23,52 +21,40 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const ProductList = () => {
+const ProductList = props => {
   const classes = useStyles();
-  const [products] = useState(data);
+
+  useEffect(() => {
+    props.fetchEvents();
+  }, []);
 
   return (
-    <Page
-      className={classes.root}
-      title="Products"
-    >
+    <Page className={classes.root} title="Products">
       <Container maxWidth={false}>
         <Toolbar />
         <Box mt={3}>
-          <Grid
-            container
-            spacing={3}
-          >
-            {products.map((product) => (
-              <Grid
-                item
-                key={product.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <ProductCard
-                  className={classes.productCard}
-                  product={product}
-                />
+          <Grid container spacing={3}>
+            {props.events.map((event, i) => (
+              <Grid item key={i} lg={4} md={6} xs={12}>
+                <EventCard data={event} />
               </Grid>
             ))}
           </Grid>
         </Box>
-        <Box
-          mt={3}
-          display="flex"
-          justifyContent="center"
-        >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
+        <Box mt={3} display="flex" justifyContent="center">
+          <Pagination color="primary" count={3} size="small" />
         </Box>
       </Container>
     </Page>
   );
 };
 
-export default ProductList;
+const mapState = state => ({
+  events: state.events
+});
+const mapDispatch = {
+  ...eventAPI
+};
+const connector = connect(mapState, mapDispatch);
+
+export default connector(ProductList);
