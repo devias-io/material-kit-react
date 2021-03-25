fe425a92eeb85144219c9ebc4d2c3e0162dc0fcb
Andrei Ionescu <andrei.ionescu3535@gmail.com>
2021-03-25 11:41:06 +0200

    add eslint airbnb style guide and fix eslint errors

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Logo = (props) => {
-  return (
-    <img
-      alt="Logo"
-      src="/static/logo.svg"
-      {...props}
-    />
-  );
-};
+const Logo = (props) => (
+  <img
+    alt="Logo"
+    src="/static/logo.svg"
+    {...props}
+  />
+);
 
 export default Logo;
