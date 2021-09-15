f18a71da540966f7e7134960e4a845ccded6b0a8
Albi Fauzi <32414473+binotby@users.noreply.github.com>
2021-09-15 12:56:09 +0700

    fix missing sidebar on hd laptop

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -191,7 +191,7 @@ const DashboardSidebar = ({ onMobileClose, openMobile }) => {
           {content}
         </Drawer>
       </Hidden>
-      <Hidden xlDown>
+      <Hidden lgDown>
         <Drawer
           anchor="left"
           open
