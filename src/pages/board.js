07a211931f0d4d1993035871868a7499d436653c
CHEZ-NOTEBOOK\chez <ehrms7989@naver.com>
2022-03-22 19:10:01 +0900

    이게 왜 누락 됬는지 모르겠으나, 누락되어서 추가

---
+++ src/pages/board.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { Box, Container } from '@mui/material';
+import { BoardListResults } from '../components/board/board-list-results';
+import { BoardListToolbar } from '../components/board/board-list-toolbar';
+import { DashboardLayout } from '../components/dashboard-layout';
+import { boards } from '../__mocks__/boards';
+
+const Boards = () => (
+  <>
+    <Head>
+      <title>
+        Board | chester lee (Material Kit)
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth={false}>
+        <BoardListToolbar />
+        <Box sx={{ mt: 3 }}>
+          <BoardListResults boards={boards} />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
+Boards.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Boards;
