bcbffe30c146370a8ea02a8bdea4246cb71c8ad1
Sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2022-03-26 21:01:27 +0530

    added report click

--- src/components/customer/customer-list-toolbar.js
+++ src/components/customer/customer-list-toolbar.js
@@ -10,8 +10,17 @@ import {
 import { Search as SearchIcon } from '../../icons/search';
 import { Upload as UploadIcon } from '../../icons/upload';
 import { Download as DownloadIcon } from '../../icons/download';
+import router from 'next/router';
 
-export const CustomerListToolbar = (props) => (
+export const CustomerListToolbar = (props) => {
+
+  function onClickHandle(e) {
+    router.push('/report');
+    console.log('You clicked');
+  }
+
+
+  return (
   <Box {...props}>
     <Box
       sx={{
@@ -44,8 +53,9 @@ export const CustomerListToolbar = (props) => (
         <Button
           color="primary"
           variant="contained"
+          onClick={onClickHandle}
         >
-          Add Customers
+          Add Reports
         </Button>
       </Box>
     </Box>
@@ -76,3 +86,4 @@ export const CustomerListToolbar = (props) => (
     </Box>
   </Box>
 );
+            }
