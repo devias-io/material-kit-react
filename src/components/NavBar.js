49d09a0ebaac879529eaabb1638fd1c47e861200
a13570049668@gmail.com <16az31@queensu.ca>
2023-10-12 19:36:03 -0400

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
         <li>Contact</li>
         {/* Add other navigation items */}
       </ul>
-      <Link href="../pages/auth/login" passHref> {/* Add Link component with href prop */}
+      <Link href="/auth/login" passHref> {/* Add Link component with href prop */}
         <button className="get-started">Get Started</button>
       </Link> {/* Close Link component */}
     </nav>
